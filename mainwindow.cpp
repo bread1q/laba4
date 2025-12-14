d79d7ecb31829c8ae40be404b4abc51f66227035
bread1q <osnova326@gmail.com>
2025-12-14 18:54:06 +0500

    Добавлена реализация базового класса Shape

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,47 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 }
 
+Shape::Shape(int x, int y) : x_(x), y_(y), color_(Qt::red), selected_(false) {}
+
+void Shape::move(int dx, int dy) {
+    x_ += dx;
+    y_ += dy;
+}
+
+// потом изменить
+bool Shape::checkBorder(int minX, int minY, int maxX, int maxY) const {
+    return true;
+}
+
+bool Shape::getSelected() const {
+    return selected_;
+}
+
+void Shape::setSelected(bool selected) {
+    selected_ = selected;
+}
+
+QColor Shape::getColor() const {
+    return color_;
+}
+
+void Shape::setColor(const QColor &color) {
+    color_ = color;
+}
+
+int Shape::getX() const {
+    return x_;
+}
+
+int Shape::getY() const {
+    return y_;
+}
+
+void Shape::setPosition(int x, int y) {
+    x_ = x;
+    y_ = y;
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,43 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+class Shape
+{
+protected:
+    int x_;
+    int y_;
+    QColor color_;
+    bool selected_;
+
+public:
+    Shape(int x, int y);
+    virtual ~Shape() = default;
+
+    virtual void draw(QPainter &painter) const = 0;
+    virtual bool contains(int x, int y) const = 0;
+    virtual QRect getBorderRect() const = 0;
+
+    virtual void move(int dx, int dy);
+    virtual bool checkBorder(int minX, int minY, int maxX, int maxY) const;
+
+    bool getSelected() const;
+    void setSelected(bool selected);
+
+    QColor getColor() const;
+    void setColor(const QColor& color);
+
+    int getX() const;
+    int getY() const;
+    void setPosition(int x, int y);
+};
 
 class MainWindow : public QMainWindow
 {
